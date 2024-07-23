503c3f2b7930410aeb3478fafb0250cfde149183
Marat Fakhreev <marat.fakhreev@workato.com>
2024-07-23 13:25:49 +0400

    fix(): loading svgs

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,12 @@
 module.exports = {
-  crossorigin: 'anonymous',
+  crossorigin: "anonymous",
   devServer: {
-    allowedHosts: 'all'
+    allowedHosts: "all",
   },
   pluginOptions: {
     express: {
       shouldServeApp: true,
-      serverDir: './server'
-    }
+      serverDir: "./server",
+    },
   },
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg');
-
-    svgRule.uses.clear();
-
-    svgRule
-      .use('svg-url-loader')
-      .loader('svg-url-loader')
-  }
 };
